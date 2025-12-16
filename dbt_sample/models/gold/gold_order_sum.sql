9337b5d749dc08b9ad2bf2251b0fa923d5906f7b
Alva Ngo <nhatanhworkacc@gmail.com>
2025-12-16 18:55:45 +0700

    Initial dbt project structure

---
+++ dbt_sample/models/gold/gold_order_sum.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='view') }}
+
+select 
+    order_date, 
+    count(*) as orders, 
+    round(sum(order_amount), 2) as total_revenue
+from {{ ref('silver_orders') }}
+group by order_date
